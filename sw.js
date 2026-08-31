bbca26bc877af421251a008303950d2686c3d0c7
vishalrahane94-tech <vishalrahane94@gmail.com>
2026-08-31 23:25:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker installed');
+const CACHE_NAME = 'gurukrupa-v2';
+const urlsToCache = ['/gurukrupa-1/', '/gurukrupa-1/index.html', '/gurukrupa-1/icon-192x192.png', '/gurukrupa-1/icon-512x512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
